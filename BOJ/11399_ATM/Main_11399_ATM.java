29fa844c70b6d1a1c947afcf16b894501c342a5d
zhye78 <ccoje2155@naver.com>
2021-02-16 22:17:35 +0900

    백준 1541, 11399, 13305

---
+++ BOJ/11399_ATM/Main_11399_ATM.java
@@ -0,0 +1,30 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_11399_ATM {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(arr);
+		
+		int sum = 0;
+		int answer = 0;
+		for(int a : arr) {
+			sum += a;
+			answer += sum;
+		}
+		System.out.println(answer);
+	}
+
+}
