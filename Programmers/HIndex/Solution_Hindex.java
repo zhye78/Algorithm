6ca5876b7d88f52a1bf58f290b1f8a6f285f08d3
zhye78 <ccoje2155@naver.com>
2021-02-19 00:16:56 +0900

    프로그래머스 H-Index

---
+++ Programmers/HIndex/Solution_Hindex.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        int len = citations.length;
+        Arrays.sort(citations);
+        
+        for(int i=len-1; i>=0; i--){
+            if(citations[i] > answer) answer++;
+            else break;
+        }
+        
+        return answer;
+    }
+}
